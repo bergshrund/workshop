4467d267a8c7058795aac0cebec5fb73b7b760c1
Andrew Ivanov <bergshrund@gmail.com>
2022-06-15 16:42:05 +0300

    Remove ip address binding

--- src/task1.js
+++ src/task1.js
@@ -1,9 +1,2 @@
-for (let i=0;i<12;i++) {
- console.log("Hello" + i);
-}
+console.log("Simple output");
 
-console.log("After all");
-console.log("After all1");
-console.log("After all1");
-console.log("After all1");
-console.log("After all1");
